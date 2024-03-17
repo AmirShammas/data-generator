5078c0bd04ee63ca083d351affb48bf5d1e869f6
ShammasAmir <amirshammas@gmail.com>
2024-03-17 22:22:17 +0330

    Correct sample_settings.

--- sample_settings.py
+++ sample_settings.py
@@ -1,9 +1,9 @@
 DATABASE = {
     'file_name': '',
-    'number_of_orders': 100,
-    'number_of_users': 300,
-    'number_of_products': 1000,
-    'price_min': 50000,
-    'price_max': 1000000,
-    'product_count_max': 20,
+    'number_of_orders': '',
+    'number_of_users': '',
+    'number_of_products': '',
+    'price_min': '',
+    'price_max': '',
+    'product_count_max': '',
 }
